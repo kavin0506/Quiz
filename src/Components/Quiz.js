93bd45539d732ca2c21fdc60d36d77cc11d9326b
kavin0506 <kavinmoorthy@gmail.com>
2024-04-04 18:37:24 +0530

    update

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,29 +1,72 @@
+import { useEffect, useState } from "react";
 import "./quiz.css";
+import data from '../Components/Data.json'
 
 const Quiz=()=>{
+    const[currentQus,setCurrentQus]=useState(0);
+    const[showScore,setShowScore]=useState(false);
+    const[score,setScore]=useState(0);
+    const[timer,setTimer]=useState(10);
+
+    useEffect(()=>{
+    let intervel;
+        if(timer>0 && !showScore){
+         intervel=setInterval(()=>{
+                setTimer((prevTime)=>prevTime-1);
+            },1000)
+        }
+        else{
+            clearInterval(intervel);
+            setShowScore(true)
+            
+        }
+        return () => clearInterval(intervel)
+    },[showScore,timer])
+
+    const handleAnswer=(selectOption)=>{
+        if(selectOption===data[currentQus].correctAns){
+            setScore((lastScore)=>lastScore+1)
+        }
+        if(currentQus<data.length-1){
+            setCurrentQus((prevQuestion)=>prevQuestion+1);
+            setTimer(10)
+        }
+        else{
+            setShowScore(true)
+        }
+    }
+    const handleRestart=()=>{
+        setCurrentQus(0);
+        setScore(0);
+        setTimer(0);
+        setShowScore(false)
+    }
     return(
         <>
         <div className="container">
-            <div className="scoreCard " style={{display:"none"}}>
-                <h2>Your Score :3/3</h2>
-                <button>Restart</button>
-            </div>
-                <div className="questionSec">
-                    <h2>Question 1</h2>
-                    <p>This is sample question</p>
+                {
+                    showScore?(<div className="scoreCard ">
+                    <h2>Your Score :{score}/{data.length}</h2>
+                    <button onClick={handleRestart}>Restart</button>
+                </div>):( <div className="questionSec">
+                    <h2>Question {currentQus+1}</h2>
+                    <p>{data[currentQus].question}</p>
                     <div className="answerOption">
-                        <button>Option 1</button>
-                        <button>Option 2</button>
-                        <button>Option 3</button>
-                        <button>Option 4</button>
+                        {
+                            data[currentQus].option.map((value,index)=>(
+                                <button key={index} onClick={()=>handleAnswer(value)}>{value}</button>
+                            ))
+                        }
                     <div className="timer">
-                        <p>Time Left:10s</p>
+                        <p>Time Left: {timer}s</p>
 
                     </div>
                     </div>
 
                 </div>
-
+)
+                }
+               
         </div>
         </>
     )
